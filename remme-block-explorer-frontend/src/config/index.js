884a11143016f7c3900a2f89bb9e1c90184ebf33
Unknown <yelagins@gmail.com>
2018-09-19 14:24:49 +0300

    update config

--- remme-block-explorer-frontend/src/config/index.js
+++ remme-block-explorer-frontend/src/config/index.js
@@ -5,8 +5,8 @@ const HOST = 'http://localhost:3005';
 //   blocksTimestamp: `/api/block-info`,
 //   transactions: `/api/transactions`,
 //   state: `/api/state`,
-//   generateAccount: `/api/generate-account`,
-//   faucet: `/api/faucet`,
+//   keysGenerator: `https://blockexplorer.remme.io/api/generate-account`,
+//   faucet: `http://46.101.18.109/api/faucet`,
 //   coinmarketcap: 'https://api.coinmarketcap.com/v2/ticker/2546/'
 // };
 
@@ -15,7 +15,7 @@ export const urls = {
   blocksTimestamp: `/api/block-info`,
   transactions: `/api/transactions`,
   state: `/api/state`,
-  keysGenerator: `https://blockexplorer.remme.io/api/generate-account`,
-  faucet: `http://46.101.18.109/api/faucet`,
+  keysGenerator: `/api/generate-account`,
+  faucet: `/api/faucet`,
   coinmarketcap: 'https://api.coinmarketcap.com/v2/ticker/2546/'
 };
